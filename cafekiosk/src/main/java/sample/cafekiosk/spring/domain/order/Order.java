91ac9b0043bd9ea45ff2f6adb57e9851f44ac9f0
lango <xmun777@naver.com>
2023-08-22 17:21:21 +0900

    feat: Stubbing with Mockito 1. OrderStatisticsService with test apply for send mail 2. send mail history(log) save 3. Order contructor refectoring

--- cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.domain.order;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -35,13 +36,22 @@ public class Order extends BaseEntity {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    public Order(List<Product> products, LocalDateTime registeredDateTime) {
-        this.orderStatus = INIT;
+    @Builder
+    private Order(List<Product> products, OrderStatus orderStatus, LocalDateTime registeredDateTime) {
+        this.orderStatus = orderStatus;
         this.totalPrice = calculateTotalPrice(products);
         this.registeredDateTime = registeredDateTime;
         this.orderProducts = products.stream()
                 .map(product -> new OrderProduct(this, product))
                 .collect(Collectors.toList());
+    }
+
+    public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
+        return Order.builder()
+                .orderStatus(INIT)
+                .products(products)
+                .registeredDateTime(registeredDateTime)
+                .build();
     }
 
     private int calculateTotalPrice(List<Product> products) {
@@ -50,8 +60,4 @@ public class Order extends BaseEntity {
                 .sum();
     }
 
-
-    public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
-        return new Order(products, registeredDateTime);
-    }
 }
