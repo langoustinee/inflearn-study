abc1083a46680fa1a7ff3a7866f9cac7dfe1d3fd
langoustinee <xmun777@naver.com>
2023-11-10 23:01:35 +0900

    feat: Spring의 DI를 통해 HelloController에 HelloService와의 의존관계 주입하기

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,10 +3,13 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 public class HelloController {
+    private final HelloService helloService;
 
-    public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
-        return helloService.sayHello(Objects.requireNonNull(name));
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
     }
 
+    public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
 }
