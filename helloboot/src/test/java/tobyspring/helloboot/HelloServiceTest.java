66cc72fd16c93fe6c4f956375d78dedf9bf9c273
langoustinee <xmun777@naver.com>
2023-11-28 23:56:17 +0900

    feat: 단위 테스트 케이스 범위로 사용할 메타 애노테이션 만들어 적용하기

--- helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,12 +3,29 @@ package tobyspring.helloboot;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest {
+}
 
 public class HelloServiceTest {
 
     @DisplayName("SimpleHelloService의 sayHello 메소드를 호출하여 결과를 응답받는다.")
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
