5733b041e08f5a020aaf9738fde2e2e8b0041d59
langoustinee <xmun777@naver.com>
2024-04-04 00:33:17 +0900

    feat: @Conditional 학습테스트 코드 작성하기
    - 조건부로 원하는 빈을 등록하거나 등록하지 않도록 하는 @Conditional 구성정보 학습
    - 조건부 @Conditional을 메타 어노테이션으로 확장하여 손쉽게 사용할 수 있음.

---
+++ helloboot/src/test/java/tobyspring/study/ConditionalTest.java
@@ -0,0 +1,104 @@
+package tobyspring.study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.GreaterThan;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConditionalTest {
+
+    @Test
+    @DisplayName("Config1 빈을 사용하도록 Conditional을 확장하면 정상적으로 빈이 등록된다.")
+    void conditionalTrue() {
+        // true
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+    }
+
+    @Test
+    @DisplayName("Config2 빈을 사용하지 않도록 Conditional을 확장하면 빈은 등록되지 않는다.")
+    void conditionalFalse() {
+        // false
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config1.class);
+                });
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(TrueCondition.class)
+    @interface TrueConditional {}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(FalseCondition.class)
+    @interface FalseConditional {}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(BooleanCondition.class)
+    @interface BooleanConditional {
+         boolean value();
+    }
+
+    @Configuration
+    @BooleanConditional(true)
+    static class Config1 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    @Configuration
+    @BooleanConditional(false)
+    static class Config2 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    static class MyBean { }
+
+    static class TrueCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
+
+    static class FalseCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
+
+    static class BooleanCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
+            Boolean value = (Boolean) annotationAttributes.get("value");
+            return value;
+        }
+    }
+
+}
