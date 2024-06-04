d723c957272da9f41a8292af0ae421582c045cf2
langoustinee <xmun777@naver.com>
2024-06-04 20:40:34 +0900

    ✨feat: 메인 함수에 Calculator 적용

--- sample-calculator/src/main/java/org/example/sample/SampleCalCulatorApplication.java
+++ sample-calculator/src/main/java/org/example/sample/SampleCalCulatorApplication.java
@@ -11,13 +11,7 @@ public class SampleCalCulatorApplication {
         long num1 = Long.parseLong(parts[0]);
         long num2 = Long.parseLong(parts[2]);
         String operator = parts[1];
-        long answer = switch (operator) {
-            case "+" -> num1 + num2;
-            case "-" -> num1 - num2;
-            case "*" -> num1 * num2;
-            case "/" -> num1 / num2;
-            default -> throw new InvalidOperatorException();
-        };
+        long answer = new Calculator().calculate(num1, operator, num2);
         System.out.println(answer);
     }
 }
